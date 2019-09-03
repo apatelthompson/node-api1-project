fc9b26bd0f27ef6a17e00dd80fadea2a53227642
avni <avni@hellorosey.com>
2019-09-03 13:44:55 -0700

    created requested API calls

--- index.js
+++ index.js
@@ -1,1 +1,132 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db.js");
+
+// let nextId = 3;
+//
+// const users = [
+//   {
+//     id: 1,
+//     name: "Samwise Gamgee",
+//     bio: "Gardener and poet. Married to Rose Cotton"
+//   },
+//   {
+//     id: 2,
+//     name: "Frodo Baggins",
+//     bio: "The ring bearer"
+//   }
+// ];
+
+const server = express();
+server.use(express.json());
+
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => res.status(200).json(users))
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved" });
+    });
+});
+
+server.post("/api/users", (req, res) => {
+  console.log(req.body);
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    res.status(400).json({ error: "Requires name and bio" });
+  }
+  db.insert({ name, bio })
+    .then(({ id }) => {
+      db.findById(id)
+        .then(user => {
+          res.status(201).json(user);
+        })
+        .catch(err => {
+          console.log(err);
+          res
+            .status(500)
+            .json({ error: "The user information could not be retrieved" });
+        });
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved" });
+    });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(user => {
+      console.log("user", user);
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ error: "The user with the specified ID does not exist" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "The user information could not be retrieved" });
+    });
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(deleted => {
+      console.log(deleted);
+      if (deleted) {
+        res.status(204).end();
+      } else {
+        res
+          .status(404)
+          .json({ error: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "The user could not be removed" });
+    });
+});
+
+server.put("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, bio } = req.body;
+  if (!name && !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  }
+  db.update(id, { name, bio })
+    .then(updated => {
+      if (updated) {
+        db.findById(id)
+          .then(user => res.status(200).json(user))
+          .catch(err => {
+            console.log(err);
+            res
+              .status(500)
+              .json({ error: "The user information could not be modified." });
+          });
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "The user could not be removed" });
+    });
+});
+
+server.listen(8000, () => console.log("API running on port 8000"));
